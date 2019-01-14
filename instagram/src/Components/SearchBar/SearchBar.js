af52c7c653e8dd33b58c8a040a56d9b8273a86af
bhumip214 <bhumip214@gmail.com>
2019-01-14 13:52:42 -0800

    Created search bar component and css stlying

---
+++ instagram/src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./SearchBar.css";
+
+const SearchBar = () => {
+  return (
+    <div className="searchBar-container">
+      <div className="logo-header">
+        <i className="logo fab fa-instagram" />
+        <hr />
+        <h1>Instagram</h1>
+      </div>
+      <div className="search-input">
+        <input type="text" placeholder=" Search" />
+      </div>
+      <div className="searchBar-icons">
+        <div className="social">
+          <i className="far fa-compass" />
+        </div>
+        <div className="social">
+          <i className="far fa-heart" />
+        </div>
+        <div className="social">
+          <i className="far fa-user" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
